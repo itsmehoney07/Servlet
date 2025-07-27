9e21a229e6af3a9410226899021df08d1a7feb06
itsmehoney07 <harshitnathsubhi.07@gmail.com>
2025-07-28 00:15:45 +0530

    some additional information

--- src/main/java/com/learning/App.java
+++ src/main/java/com/learning/App.java
@@ -5,6 +5,8 @@ import org.apache.catalina.Context;
 import org.apache.catalina.LifecycleException;
 import org.apache.catalina.startup.Tomcat;
 
+import java.io.File;
+
 /**
  * Hello world!
  *
@@ -20,7 +22,15 @@ public class App
         // as we are using embedded tomcat we have to do the configuration here manually
         Tomcat tomcat = new Tomcat();
         //for servlet mapping we have to open the context
-        Context context= tomcat.addContext("",null);
+        // here the addContext is having 2 parameters
+        // contextPath: where application will be available , empty means http://localhost:8080
+        // if the context path is having "hello" then it means application is available at this http://localhost:8080/hello
+        // docBase : telling tomcat that all the files will be available at this path
+        // tomcat will look for all the files like html , jsp and servlet in this location
+        Context context= tomcat.addContext("",new File(".").getAbsolutePath());
+        // simple meaning if you are holding a party contextPath is the invite shared
+        // and docBase is the address where party is hosted
+        // if invite is send and address is missing now one will show up right..
         Tomcat.addServlet(context, "HelloServelet",new HelloServelet());
         context.addServletMappingDecoded("/hello", "HelloServelet");
 
